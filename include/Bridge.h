5d5dc0f407b6854d8e163397d1f1dd5f1087a1b9
Oliver Arnold <oarnold@nanocosmos.de>
2017-01-11 16:19:44 +0100

    add nanostreamAVC library and Bintu framework

---
+++ include/Bridge.h
@@ -0,0 +1,19 @@
+//
+//  Bridge.h
+//  BintuStream
+//
+//  Created by Rene Wetzlar on 08/07/16.
+//  Copyright © 2016 Nanocosmos. All rights reserved.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+
+#endif /* Bridge_h */
+
+#import "nanostreamAVC.h"
+#import "nanostreamAVCRtmpSourceCaptureSession.h"
+#import "nanoLicenseConfig.h"
+#import <Bintu/Bintu.h>
+#import "IASKAppSettingsViewController.h"
---
+++ include/nanoLicenseConfig.h
@@ -0,0 +1,21 @@
+/**
+  @file nanoLicenseConfig.h
+  nanoStream and bintu.live license and api key
+
+  @copyright (c) 2015-2016 nanocosmos. All rights reserved.
+  http://www.nanocosmos.de
+  This is proprietary code and requires a valid usage license
+*/
+
+//#error TODO: enter your license and API keys here!
+
+// nanoStream license key ("nlic:...")
+static NSString * const myLicenseKey =
+@""
+;
+
+// bintu.live API key
+static NSString * const myApiKey  =
+@""
+;
+
